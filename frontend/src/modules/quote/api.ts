84495f43efb40b605feb7e7648edbd4e6ee4cb90
kangray <kangray@sheridancollege.ca>
2021-06-07 10:31:27 -0400

    added data type

--- frontend/src/modules/quote/api.ts
+++ frontend/src/modules/quote/api.ts
@@ -1,13 +1,27 @@
 import axios from 'axios'
-import { Quote } from './types';
-import { CreateQuotePayload } from './types';
+import i18next from 'i18next';
 import { getServerUrl } from '../../lib/utils';
+import { Quote, QuoteOption } from '../types';
 
+export type CreateQuotePayload = {
+    userType: string, //replace with Customer | Vendor
+    asset: string,
+    applicationAmount: number,
+    leaseType: string,
+    contactName: string,
+    contactEmail: string,
+    vendorName: string,
+    vendorEmail: string,
+    vendorBusinessName: string,
+    quoteOption: QuoteOption[]
+}
 
-export const createQuote = async(payload: CreateQuotePayload): Promise<Quote> => {
+//For view quote
+export const getQuote = async(portalId: number | string): Promise<Quote | Error> => {
     try{
-        const { data } = await axios.post<Quote>(`${getServerUrl}/v2/quote`, payload, { withCredentials: true });
-        return data as Quote
+        //replace with server url when updated
+        const { data } = await axios.get<Quote>(`${getServerUrl}/quotes/${portalId}}`);
+        return data as Quote;
     }catch(error){
         if(axios.isAxiosError(error) && error.response){
             throw error;
--- frontend/src/modules/types.ts
+++ frontend/src/modules/types.ts
@@ -5,3 +5,44 @@ export type User = {
   createdAt: string;
   updatedAt: string;
 };
+
+//temporary proposed data models
+export type Quote = {
+  quoteId: string;
+  asset: string;
+  applicationAmount: number;
+  quoteOptions: QuoteOption[];
+  quoteExpiryDate: string;
+}
+
+export type QuoteOption = {
+  monthlyAmount: number;
+  term: '12M' | '24M' | '36M' | '48M' | '60M' | '72M'
+  financeRate: number;
+  purchaseOptionDate; string;
+}
+
+export type CreditApplication = {
+  creditAppNumber: string;
+  createdDate: string;
+  applcicationAmount: number;
+  description: string;
+  creditStatus: string;
+  applicationStatus: string;
+  quoteId: string;
+  asset: string;
+}
+
+export type Lease = {
+  leaseNumber: string;
+  leaseStartDate: string;
+  fullTerm: number;
+  vendorName: string;
+  vendorInvoice: number;
+  assets: string;
+  paymentBeforeTax: number;
+  purchaseOptionDate: string;
+  optionAmount: number;
+  leaseEndDate: string;
+  quoteId: string;
+}
