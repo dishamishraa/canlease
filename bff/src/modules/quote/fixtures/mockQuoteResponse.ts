3ce519ca8e6cf864f67b7930cc6047c0f0a881be
Riya Anadkat <riya@Riyas-MacBook-Pro.local>
2021-06-17 10:56:12 -0400

    changing expiry date

--- bff/src/modules/quote/fixtures/mockQuoteResponse.ts
+++ bff/src/modules/quote/fixtures/mockQuoteResponse.ts
@@ -30,7 +30,7 @@ const mockResponse: Quote = {
       purchaseOptionDate: '2021-06-14T19:23:28.744Z',
     },
   ],
-  quoteExpiryDate: '2021-06-14T19:23:28.744Z',
+  quoteExpiryDate: '2021-06-22T19:23:28.744Z',
 };
 
 export default mockResponse;
