6a7a0ca932c7a5ad8b710676e6ecff378bf5262a
Riya Anadkat <riya.anadkat@redthreadinnovations.com>
2021-07-05 09:06:27 -0400

    figma fixes

--- frontend/src/components/atoms/Select/Select.tsx
+++ frontend/src/components/atoms/Select/Select.tsx
@@ -35,8 +35,7 @@ const Select: React.FC<SelectProps> = ({
   text,
   icon,
   className,
-}) => {
-  return (
+}) => (
     <button
       type={selectType}
       onClick={onSelectClicked}
@@ -48,8 +47,7 @@ const Select: React.FC<SelectProps> = ({
         className={styles.icon}
         {...icon} />
     </button>
-  );
-};
+);
 
 Select.defaultProps = defaultProps;
 
