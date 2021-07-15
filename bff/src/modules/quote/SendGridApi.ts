8d9ccdfe5f55b4aeb3230dadfefad0e62054e4f8
Nikita Kramarovsky <nikita.kramarovsky@redthreadinnovations.com>
2021-07-15 14:16:30 -0400

    fixed typo

--- bff/src/modules/quote/SendGridApi.ts
+++ bff/src/modules/quote/SendGridApi.ts
@@ -23,7 +23,7 @@ export default class SendGridApi {
           company_name: companyName,
           submitted_by: submittedBy,
           action_url: `${FRONTEND_URL}/instaQuote/${quoteId}`,
-          sign_up_link:  `${FRONTEND_URL}/account/signUp}`,
+          sign_up_link:  `${FRONTEND_URL}/account/signUp`,
         },
         templateId: SENDGRID_QUOTE_TEMPLATE_ID,
       };
