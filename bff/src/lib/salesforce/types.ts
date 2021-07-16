62c3fd54cb25afaae7c91fd9d36fc5b4f9be4687
Thomas Sutlovic <thomas.sutlovic@redthreadinnovations.com>
2021-07-16 12:40:18 +0200

    linting fix

--- bff/src/lib/salesforce/types.ts
+++ bff/src/lib/salesforce/types.ts
@@ -82,9 +82,11 @@ export type CreateQuoteVendor = CreateQuoteCustomer & {
   vendorBusinessName: string;
 };
 
-export const isCreateQuoteCustomer = (payload: CreateQuote): payload is CreateQuoteCustomer => (!payload.vendorName);
+export const isCreateQuoteCustomer = (payload: CreateQuote):
+  payload is CreateQuoteCustomer => (!payload.vendorName);
 
-export const isCreateQuoteVendor = (payload: CreateQuote): payload is CreateQuoteVendor => (!!payload.vendorName);
+export const isCreateQuoteVendor = (payload: CreateQuote):
+  payload is CreateQuoteVendor => (!!payload.vendorName);
 
 export type QuoteOption = {
   monthlyAmount: number;
