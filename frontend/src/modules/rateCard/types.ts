f1983209cdf428327fecb0db1c9c729c487931a4
Riya Anadkat <riya.anadkat@redthreadinnovations.com>
2021-07-21 09:45:30 -0400

    renamed ratecard folder

--- frontend/src/modules/rateCard/types.ts
+++ frontend/src/modules/rateCard/types.ts
@@ -1,25 +1,10 @@
-export type RateCardPayload = {
-  uuid: string;
-  cardtype: string;
-};
-
 export type RateCard = {
-  id: number;
-  uuid: string;
-  cardtype: string;
-  createdAt: string;
-  updatedAt: string;
+    id: number;
+    uuid: string;
+    cardtype: string;
+    createdAt: string;
+    updatedAt: string;
 };
 
-export type Rate = {
-  id: number;
-  uuid: string;
-  term: number;
-  minmonthlyreturn: number;
-  maxmonthlyreturn: number;
-  regularir: number;
-  tenatendir: number;
-  ratecardid: number;
-  createdAt: string;
-  updatedAt: string;
-};
+export type CreateRateCard = Pick<RateCard, 'cardtype'>;
+
