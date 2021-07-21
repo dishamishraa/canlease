6341d8c62e7f98d214538e2a85d81bdb3f251835
Riya Anadkat <riya.anadkat@redthreadinnovations.com>
2021-07-21 09:49:18 -0400

    added back removed types

--- frontend/src/modules/rateCard/types.ts
+++ frontend/src/modules/rateCard/types.ts
@@ -1,9 +1,27 @@
+export type RateCardPayload = {
+  uuid: string;
+  cardtype: string;
+};
+
 export type RateCard = {
     id: number;
     uuid: string;
     cardtype: string;
     createdAt: string;
     updatedAt: string;
+};
+
+export type Rate = {
+  id: number;
+  uuid: string;
+  term: number;
+  minmonthlyreturn: number;
+  maxmonthlyreturn: number;
+  regularir: number;
+  tenatendir: number;
+  ratecardid: number;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export type CreateRateCard = Pick<RateCard, 'cardtype'>;
